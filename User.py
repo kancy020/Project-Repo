63dd13e2c1f65c61965d76ba91929e0bc8f1ff64
Loganccopeland <134029405+Loganccopeland@users.noreply.github.com>
2024-09-03 15:00:36 +0930

    users class started commiting to get to my pc

---
+++ User.py
@@ -0,0 +1,7 @@
+class User:
+
+    def __init__(self,username: str ,password:str,contactNumber:int,homeLocations: list) -> None:
+        self.username= username
+        self.password =password
+        self.contactNumber = contactNumber
+        self.homeLocations= homeLocations
