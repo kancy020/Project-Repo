778e3cd0626b1222d65acdc6764c987a9b7fb2ed
klejt002 <klejt002@mymail.unisa.edu.au>
2024-09-04 15:03:09 +0930

    Built on app.py file
    Working on getting a functioning database using SQLAlchemy

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stores.db'
+# Initalise DB
+db = SQLAlchemy(app)
 
-@app.route('/add_store', methods=['GET','POST'])
+# create db model
+class Stores(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.id
+
+@app.route('/addStore', methods=['GET','POST'])
 def add_store():
     pass
 
-@app.route('/stores')
+@app.route('/storesDisplay')
 def list_stores():
     pass
--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 class Store:
-    def __init__(self, name, address):
+    def __init__(self, id, name, address, date_created):
+        self.id = id
         self.name = name
         self.address = address
-
+        
 class MenuItem:
     def __init__(self, name, price, store):
         self.name = name
