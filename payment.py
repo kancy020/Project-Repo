7e742220d57e113e6ed790371fb5aa08e57791d2
JordonCursaro <Jorcursa@gmail.com>
2024-09-04 20:22:54 +0930

    make some new changes to classes

--- payment.py
+++ payment.py
@@ -2,11 +2,13 @@
 #testing if payment work
 class Order:
 
+    allOrder = []
+
     def __init__(self, name, price, quantity=1) -> None:
         self.name = name
         self.price = price
         self.quantity = quantity
-        #Payment.add_order(self) 
+        Order.allOrder.append(self)
 
     '''set the total price for item incase there amount of items being processed'''
     def get_total_price(self):
@@ -19,13 +21,8 @@ class Payment:
     def __init__(self, balance) -> None:
         self.totalPrice = 0
         self.__balance = balance
-        self.items = []
+        self.items = Order.allOrder.copy()
     
-    
-    #add the given order to the class
-    # @classmethod
-    # def add_order(cls, order):
-    #     cls.items.append(order)
 
     '''adds the orders to items list to calculate total price'''
     def add_item(self, order):
@@ -39,7 +36,9 @@ class Payment:
             print(result)
         else:
             self.receipt()
+            #this ensure the lists are cleared for each transaction
             self.items.clear()
+            Order.allOrder.clear()
 
     '''calculates the total price for order'''
     def calculate_total_price(self):
@@ -62,6 +61,7 @@ class Payment:
         print()
         print("---------------------------")
         print(f"Total: ${self.totalPrice}")
+        print()
         for items in self.items:
             print(f"Item Name: {items.name}\nPrice: ${items.price}\nQuantity: {items.quantity}\n")
         print(f"balance now: {self.__balance}")
@@ -76,8 +76,8 @@ order2 = Order("Item2",20.00,2)
 
 payment = Payment(1000.00)
 
-payment.add_item(order1)
-payment.add_item(order2)
+# payment.add_item(order1)
+# payment.add_item(order2)
 
 payment.initiate_transaction()
 
@@ -86,14 +86,15 @@ order3 = Order("Item3",10.00)
 
 payment1 = Payment(1100.00)
 
-payment1.add_item(order3)
+# payment1.add_item(order3)
 
 payment1.initiate_transaction()
 
 order4 = Order("Item3",10.00)
 
+
 payment2 = Payment(1.00)
 
-payment2.add_item(order3)
+# payment2.add_item(order3)
 
 payment2.initiate_transaction()
