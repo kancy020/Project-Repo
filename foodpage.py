3947f4cd8bf5767c863cc44f334852af00d284f5
diemy006 <diemy006@mymail.unisa.edu.au>
2024-09-05 11:05:12 +0930

    Created base routes with no implementation of displaying the dishes individually yet and created the menu database model to access each dish

--- foodpage.py
+++ foodpage.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///menu.db'
+db = SQLAlchemy(app)
+
+#create db model
+class Menu(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.id
+    
+
+dish = []
 
 @app.route('/')
 def menu():
@@ -11,6 +29,11 @@ def menu():
         {'name': 'Salad', 'description': 'Fresh garden salad with a variety of veggies.', 'price': '$4.99'},
     ]
     return render_template('food.html', food_items=food_items)
+
+@app.route('/dish/<int:menu_id>')
+def dish(menu_id):
+    title = "Menu"
+    return render_template('dish.html', title=title)
 
 if __name__ == '__main__':
     app.run(debug=True)
