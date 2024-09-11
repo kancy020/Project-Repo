d927f993b3d2b2cf93831dca9b47e24240b95382
JordonCursaro <Jorcursa@gmail.com>
2024-09-11 13:25:02 +0930

    payment page - list orders

--- app.py
+++ app.py
@@ -2,15 +2,28 @@ from flask import *
 
 app = Flask(__name__)
 
-'''not '''
+
 @app.route("/")
 def home():
     return redirect(url_for('payment_page'))
 
 @app.route('/payment')
 def payment_page():
-   return render_template("paymentPage.html")
+    orders = [
+        {"name": "pokemon", "quantity": 1, "price": 10.00},
+        {"name": "fuel", "quantity": 1, "price": 50.00},
+        {"name": "pokemon", "quantity": 1, "price": 10.00},
+        {"name": "pokemon", "quantity": 1, "price": 10.00},
+        {"name": "pokemon", "quantity": 1, "price": 10.00}
+    ]
+    return render_template("paymentPage.html", orders=orders)
 
+@app.route('/receipt')
+def receipt_page():
+    # if request.method == 'POST':
+    #     amount = request.form.get('amount')
+    #     return render_template("receipt.html",amount=amount)
+    return render_template("receipt.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
