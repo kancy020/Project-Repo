683597d012d81f02686e7cf5a710fcf0fb4794a0
JordonCursaro <Jorcursa@gmail.com>
2024-09-11 11:13:38 +0930

    payment page use case

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
+'''not '''
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return redirect(url_for('payment_page'))
+
+@app.route('/payment')
+def payment_page():
+   return render_template("paymentPage.html")
+
 
 if __name__ == "__main__":
     app.run()
