c51fae68a878ee17c1e8d5ec600f00045df71e9a
kancy020 <115684569+kancy020@users.noreply.github.com>
2024-09-11 13:13:48 +0930

    setting up flask

--- fuelPageStore.py
+++ fuelPageStore.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.py.route("/")
-def hello():
+@app.route("/")
+def index():
    return 'Hello World!'
+
+if __name__ == "__main__":
+   app.run(debug=True)
