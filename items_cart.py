b86be94a01aa035052942361c3cc4b4a5dd32171
ConnorFSA <133937825+ConnorFSA@users.noreply.github.com>
2024-09-11 19:24:20 +0930

    Refactor cartItem class: Add identifier for item type

--- items_cart.py
+++ items_cart.py
@@ -2,8 +2,9 @@ from abc import ABC, abstractmethod
 
 class cartItem(ABC):
 
-    def __init__(self, itemName, itemID, price, quantity=1):
+    def __init__(self, itemName, itemID, price, quantity, itemType = 'Item') :
         self.__itemName = itemName
         self.__itemID = itemID
         self.__price = price
         self.__quantity = quantity
+        self.__itemType = itemType
