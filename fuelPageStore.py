21fe045077d31f1e6dacbec005892f9ab6e15973
kancy020 <115684569+kancy020@users.noreply.github.com>
2024-09-30 18:11:32 +0930

    displaying petrol list via database

--- fuelPageStore.py
+++ fuelPageStore.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-   return render_template('PumpAndpayPage.html')
+   petrolTypes = [
+   {'id': 'unleaded91', 'name': 'Unleaded 91' },
+   {'id': 'unleaded95', 'name': 'Unleaded 95' },
+   {'id': 'unleaded98', 'name': 'Unleaded 98' },
+   {'id': 'diesel', 'name': 'Diesel' },
+   {'id': 'premiumDiesel', 'name': 'Premium Diesel' },
+   ]
+   return render_template('PumpAndpayPage.html', petrolTypes=petrolTypes)   
 
 if __name__ == "__main__":
    app.run(debug=True)
