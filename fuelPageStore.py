86827c3372bec3816207e93d7fdceefbd1345014
kancy020 <115684569+kancy020@users.noreply.github.com>
2024-09-30 19:16:15 +0930

    facilitating links between html pages for button presses and adding in a payment before pumping page

--- fuelPageStore.py
+++ fuelPageStore.py
@@ -2,14 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@ app.route("/choises")
+def petrolStationHome():
+   return render_template("FuelProcessPage.html")
+
+@ app.route("/petrolSelect")
+def petrolPage():
+   return render_template("PumpAndPayPage.html")
+
+@app.route("/pay")
+def payPage():
+   return render_template("PayBeforePumpPage.html")
+
 @app.route("/")
 def index():
    petrolTypes = [
-   {'id': 'unleaded91', 'name': 'Unleaded 91' },
-   {'id': 'unleaded95', 'name': 'Unleaded 95' },
-   {'id': 'unleaded98', 'name': 'Unleaded 98' },
-   {'id': 'diesel', 'name': 'Diesel' },
-   {'id': 'premiumDiesel', 'name': 'Premium Diesel' },
+   {'id': 'unleaded91', 'name': 'Unleaded 91', "price": '1.30'},
+   {'id': 'unleaded95', 'name': 'Unleaded 95', "price": '1.42'},
+   {'id': 'unleaded98', 'name': 'Unleaded 98', "price": '1.60' },
+   {'id': 'diesel', 'name': 'Diesel', "price": '1.40' },
+   {'id': 'premiumDiesel', 'name': 'Premium Diesel', "price": '1.55' },
    ]
    return render_template('PumpAndpayPage.html', petrolTypes=petrolTypes)   
 
